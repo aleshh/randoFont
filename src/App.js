8430079b18f21fe0ce947fd4e414dc9a1264ddd1
Alesh Houdek <mail@alesh.com>
2018-10-06 12:13:42 -0600

    outputs 5 random Google fonts

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
 import React, { Component } from 'react';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
-console.log('API_KEY: ', API_KEY);
 
 class App extends Component {
   state = {
-    fonts: []
+    randomFonts: [],
+    fontCount: 3
   };
+
+  getRandomFont = arr => arr[this.random(arr.length) - 1]
+  random = max => Math.floor(Math.random() * max) + 1
 
   componentDidMount() {
     fetch('https://www.googleapis.com/webfonts/v1/webfonts?key=' + API_KEY)
       .then(response => response.json())
       .then(data => {
-        console.log('data: ', data);
+        let fontCount = [];
+        for (let i = 0; i < this.state.fontCount; i++) {
+          fontCount.push(this.getRandomFont(data.items));
+        }
+        console.log('selected fonts: ', fontCount);
+        console.log(this.state.randomFonts[0]);
         this.setState({
-          fonts: data.items
-        })
+          randomFonts: fontCount
+        });
+
       });
   }
 
   render() {
-    console.log(this.state.fonts[0]);
+
     return (
       <div className="App">
         <h1>randoFont</h1>
-        {this.state.fonts.map(font => (<p key={font.family}>{font.family}</p>))}
+        {this.state.randomFonts.map(font => (<p key={font.family}>{font.family}</p>))}
       </div>
     );
   }
