4c490868810e7b7905014600c41e155c31f208fd
Alesh Houdek <mail@alesh.com>
2018-12-17 15:44:27 -0700

    Moved random font function from Fonts to Navbar

--- src/components/Fonts.js
+++ src/components/Fonts.js
@@ -2,62 +2,16 @@ import React, { Component } from 'react';
 import Font from './Font';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux'
-import WebFont from 'webfontloader';
 
-import { fetchFonts, setRandomFonts } from '../actions/fontActions'
 
 export class Fonts extends Component {
   static propTypes = {
-    allFonts: PropTypes.array,
+    // allFonts: PropTypes.array,
     randomFonts: PropTypes.array.isRequired,
     sampleSentence: PropTypes.string.isRequired,
-    categoriesWanted: PropTypes.array.isRequired,
-    fontCount: PropTypes.number.isRequired,
-    fetchFonts: PropTypes.func.isRequired
-  }
-
-  componentWillMount() {
-    this.props.fetchFonts();
-  }
-
-  componentDidUpdate() {
-    if (this.props.randomFonts.length === 0) {
-      this.randomizeFonts();
-    }
-  }
-
-  getRandomFont = arr => arr[this.random(arr.length) - 1];
-  random = max => Math.floor(Math.random() * max) + 1;
-
-  randomizeFonts = () => {
-    const { categoriesWanted, fontCount, allFonts } = this.props;
-    const fontList = [];
-
-    if (allFonts.length === 0) return;
-
-    if (categoriesWanted.length === 0) {
-      setRandomFonts();
-      return;
-    }
-
-    while (fontList.length < fontCount) {
-      const font = this.getRandomFont(allFonts);
-
-      // add the font if it's the right category and it's not
-      // already in the list
-      if (categoriesWanted.includes(font.category)
-          && !fontList.includes(font)) {
-        fontList.push(font);
-      }
-    }
-
-    WebFont.load({
-      google: {
-        families: fontList.map(font => font.family)
-      }
-    });
-
-    this.props.setRandomFonts(fontList);
+    // categoriesWanted: PropTypes.array.isRequired,
+    // fontCount: PropTypes.number.isRequired,
+    // fetchFonts: PropTypes.func.isRequired
   }
 
   render() {
@@ -76,11 +30,11 @@ export class Fonts extends Component {
 }
 
 const mapStateToProps = state => ({
-  allFonts: state.fonts.allFonts,
+  // allFonts: state.fonts.allFonts,
   randomFonts: state.fonts.randomFonts,
   sampleSentence: state.fonts.sampleSentence,
-  categoriesWanted: state.fonts.categoriesWanted,
-  fontCount: state.fonts.fontCount
+  // categoriesWanted: state.fonts.categoriesWanted,
+  // fontCount: state.fonts.fontCount
 });
 
-export default connect(mapStateToProps, { fetchFonts, setRandomFonts })(Fonts);
+export default connect(mapStateToProps, {})(Fonts);
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,67 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import WebFont from 'webfontloader';
 
-import CheckboxInput from './CheckboxInput'
+import CheckboxInput from './CheckboxInput';
 
-import { setCategoriesWanted, setFontCount } from '../actions/fontActions';
+import { setCategoriesWanted, setFontCount,  fetchFonts, setRandomFonts } from '../actions/fontActions';
 
 export class Navbar extends Component {
   static propTypes = {
+    allFonts: PropTypes.array,
+    randomFonts: PropTypes.array.isRequired,
+    // sampleSentence: PropTypes.string.isRequired,
+    // categoriesWanted: PropTypes.array.isRequired,
+    // fontCount: PropTypes.number.isRequired,
+    fetchFonts: PropTypes.func.isRequired,
+
     setCategoriesWanted: PropTypes.func.isRequired,
     setFontCount: PropTypes.func.isRequired,
+  }
+
+  componentWillMount() {
+    this.props.fetchFonts();
+  }
+
+  componentDidUpdate() {
+    if (this.props.randomFonts.length === 0) {
+      this.randomizeFonts();
+    }
+  }
+
+  getRandomFont = arr => arr[this.random(arr.length) - 1];
+  random = max => Math.floor(Math.random() * max) + 1;
+
+  randomizeFonts = () => {
+    const { categoriesWanted, fontCount, allFonts } = this.props;
+    const fontList = [];
+
+    if (allFonts.length === 0) return;
+
+    if (categoriesWanted.length === 0) {
+      // this.props.setRandomFonts();
+      return;
+    }
+
+    while (fontList.length < fontCount) {
+      const font = this.getRandomFont(allFonts);
+
+      // add the font if it's the right category and it's not
+      // already in the list
+      if (categoriesWanted.includes(font.category)
+          && !fontList.includes(font)) {
+        fontList.push(font);
+      }
+    }
+
+    WebFont.load({
+      google: {
+        families: fontList.map(font => font.family)
+      }
+    });
+
+    this.props.setRandomFonts(fontList);
   }
 
   render() {
@@ -66,10 +118,12 @@ export class Navbar extends Component {
 }
 
 const mapStateToProps = state => ({
+  allFonts: state.fonts.allFonts,
+  randomFonts: state.fonts.randomFonts,
   categoriesWanted: state.fonts.categoriesWanted,
   fontCount: state.fonts.fontCount
 })
 
 export default connect(
-  mapStateToProps, { setFontCount, setCategoriesWanted }
+  mapStateToProps, { setFontCount, setCategoriesWanted, fetchFonts, setRandomFonts }
 )(Navbar);
