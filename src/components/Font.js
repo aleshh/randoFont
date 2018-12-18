15b4ccde437ab707404b2a70f3a67d086dfc08f6
Alesh Houdek <mail@alesh.com>
2018-12-18 03:16:25 -0700

    Converted Font to functional component

--- src/components/Font.js
+++ src/components/Font.js
@@ -1,41 +1,32 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types';
+import React from 'react';
 
-export class Font extends Component {
-  static propTypes = {
-    font: PropTypes.object.isRequired,
-    sampleSentence: PropTypes.string.isRequired
-  }
+function Font(props) {
+  const { font, sampleSentence } = props;
 
-  render() {
-    const { font, sampleSentence } = this.props;
-
-    return (
-      <div className="font-card">
-        <p
-          // contentEditable without a React warning in the console
-          ref={function(e){if(e != null) e.contentEditable=true;}}
-          className="sample-sentence"
-          style={{fontFamily: font.family}}
+  return (
+    <div className="font-card">
+      <p
+        // contentEditable without a React warning in the console
+        ref={function(e){if(e != null) e.contentEditable=true;}}
+        className="sample-sentence"
+        style={{fontFamily: font.family}}
+      >
+        {sampleSentence}
+      </p>
+      <p>
+        font: <span className="font-info">{font.family}</span>
+        category: <span className="font-info">{font.category}</span>
+        variants: <span className="font-info">{font.variants.length}</span>
+        <a
+          href={'https://fonts.google.com/specimen/' + font.family}
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          {sampleSentence}
-        </p>
-        <p>
-          font: <span className="font-info">{font.family}</span>
-          category: <span className="font-info">{font.category}</span>
-          variants: <span className="font-info">{font.variants.length}</span>
-          <a
-            href={'https://fonts.google.com/specimen/' + font.family}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-           Link
-          </a>
-
-        </p>
-      </div>
-      )
-  }
+          Link
+        </a>
+      </p>
+    </div>
+    )
 }
 
 export default Font;
