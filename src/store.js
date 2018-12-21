9b52240bbb8c29dc3a212867037cb440a9a2da49
Alesh Houdek <mail@alesh.com>
2018-12-21 08:55:24 -0700

    Added localStorage to store

--- src/store.js
+++ src/store.js
@@ -7,10 +7,18 @@ const initialState = {};
 const middleware = [thunk];
 
 const store = createStore(
-  rootReducer, initialState, compose(
+  rootReducer,
+  (localStorage['redux-store']) ?
+    JSON.parse(localStorage['redux-store']) :
+    initialState,
+  compose(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
+
+store.subscribe(() => {
+  localStorage['redux-store'] = JSON.stringify(store.getState());
+});
 
 export default store;
