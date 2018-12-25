c9812342aeb98b0f63b83fa2c73ff6113541b41c
Alesh Houdek <mail@alesh.com>
2018-12-25 07:57:45 -0700

    FontList displays currentlyViewedFonts instead of fonts passed to it

--- src/components/FontsList.js
+++ src/components/FontsList.js
@@ -7,14 +7,22 @@ import { setCurrentlyViewedFonts } from '../actions/fontActions';
 
 import Font from './Font';
 
+
 class FontsList extends Component {
 
   componentWillMount() {
+    // we are using this currentlyViewedFonts, assigned here, instead of using
+    // fonts passed from the parent component, so that on the FavoriteFonts
+    // screen the user can toggle a font's Favorite status without it
+    // immediately disapearing. It's removed from favoriteFonts but not
+    // currentlyViewedFonts, so it can be re-favorited if it was a mistake
     this.props.setCurrentlyViewedFonts(this.props.fonts);
   }
 
   render() {
-    const { fonts, sampleSentence, favoriteFonts, currentlyViewedFonts } = this.props;
+    const { sampleSentence, favoriteFonts, currentlyViewedFonts } = this.props;
+
+    const fonts = currentlyViewedFonts;
 
     if(fonts.length > 0) {
       WebFont.load({
