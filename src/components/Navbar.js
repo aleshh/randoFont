19198a7b1878373db1a8d13ae3d0ca82bc7be11a
Alesh Houdek <mail@alesh.com>
2019-01-22 09:09:53 -0700

    navigation only displayed when there are favorite fonts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import NavbarFavCount from './NavbarFavCount';
 
-const Navbar = () => {
-  return (
-    <div className="navbar">
-      <h1>randoFont</h1>
+const Navbar = props => {
+  const { showNavbar } = props;
+
+  const navigation = showNavbar ?
+    <React.Fragment>
       <NavbarLink to="/" label="Home" />
       <NavbarLink to="/favorites" label="Favorites" />
       <NavbarFavCount/>
+    </React.Fragment> :
+    null;
+
+  return (
+    <div className="navbar">
+      <h1>randoFont</h1>
+      { navigation }
     </div>
   )
 }
@@ -22,4 +32,12 @@ const NavbarLink = ({ to, label, ...rest }) => (
   )} />
 )
 
-export default Navbar;
+Navbar.propTypes = {
+  showNavbar: PropTypes.bool.isRequired
+};
+
+const mapStatetoProps = state => ({
+  showNavbar: (state.fonts.favoriteFonts.length > 0)
+});
+
+export default connect(mapStatetoProps, null)(Navbar);
