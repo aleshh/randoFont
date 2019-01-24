a00124a6be94967b4c8f420239bfc24c4e23c7b0
Alesh Houdek <mail@alesh.com>
2019-01-24 10:33:15 -0700

    Styled reload button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <p className="credits">
-      Built by <a href="https://alesh.com">Alesh</a>, 2018. <a href="https://github.com/aleshh/randoFont">GitHub</a>.
+      Made by <a href="https://alesh.com">Alesh</a>, 2018. <a href="https://github.com/aleshh/randoFont">GitHub</a>.
     </p>
     )
 }
