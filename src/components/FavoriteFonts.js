b21f06e3f6cdf0a1d24b8f485fd613c7d389e894
Alesh Houdek <mail@alesh.com>
2019-07-06 07:55:05 -0600

    Removed old code

--- src/components/FavoriteFonts.js
+++ src/components/FavoriteFonts.js
@@ -2,8 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-// import { setCurrentlyViewedFonts } from '../actions/fontActions';
-
 import FontsList from './FontsList';
 
 const FavoriteFonts = props => {
@@ -16,12 +14,10 @@ const FavoriteFonts = props => {
 
 FavoriteFonts.propTypes = {
   favoriteFonts: PropTypes.array.isRequired,
-  // setCurrentlyViewedFonts: PropTypes.func.isRequired
 }
 
 const mapStateToProps = state => ({
   favoriteFonts: state.fonts.favoriteFonts,
 });
 
-// export default connect(mapStateToProps, { setCurrentlyViewedFonts })(FavoriteFonts);
 export default connect(mapStateToProps, null)(FavoriteFonts);
