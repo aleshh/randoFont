74580f0e1163274f9c81b04c44c22b9a4846755e
Alesh Houdek <mail@alesh.com>
2026-06-30 10:26:45 -0600

    Update dependencies

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react({ include: /\.[jt]sx?$/ })],
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.[jt]sx?/
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx'
-      }
-    }
-  },
   build: {
     outDir: 'build'
   },
